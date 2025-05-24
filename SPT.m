2acf5579fa9409e3ed42ceb2b44f5dc1ddca8446
DiegoAriasCazco <99507135+DiegoAriasCazco@users.noreply.github.com>
2025-05-24 15:38:52 -0500

    Code for 3 or 4 objective functions
    Code for 3 or 4 objective functions

--- SPT.m
+++ SPT.m
@@ -40,9 +40,9 @@ Aeq = []; beq = [];
 
 LCmin=2*Lx+2*Ly;
 %3000
-     %LT       Dx Dy Rg  GPR Em nR Es  
-lb = [1540 0  0   0   0 0 0 0];  % Limites inferiores de variables 
-ub = [1540      Lx Ly  5  1e4 Eto 200 Est];  %Limites superiores de variables
+     %LT   Dx Dy Rg  GPR Em nR Es  
+lb = [LCmin 0  0   0   0 0 0 0];  % Limites inferiores de variables 
+ub = [1540 Lx Ly  5  1e4 Eto 100 Est];  %Limites superiores de variables
 
 x0=(lb+ub)/2;
 
@@ -59,7 +59,7 @@ options = optimoptions('gamultiobj');
 %options = optimoptions(options,'PopulationSize', PopulationSize_Data);
 %options = optimoptions(options,'ParetoFraction', ParetoFraction_Data);
 options = optimoptions(options,'MigrationDirection', 'both');
-options = optimoptions(options,'FunctionTolerance', 1e-4,'MaxStallGenerations',500);
+%options = optimoptions(options,'FunctionTolerance', 1e-4,'MaxStallGenerations',500);
 %options = optimoptions(options,'MigrationInterval', MigrationInterval_Data);
 %options = optimoptions(options,'MigrationFraction', MigrationFraction_Data);
 options = optimoptions(options,'CreationFcn', @gacreationnonlinearfeasible);
@@ -71,8 +71,8 @@ options = optimoptions(options,'HybridFcn', {  @fgoalattain [] });
 
 
 % options = optimoptions(options,'MigrationDirection', 'both');
- options = optimoptions(options,'ParetoFraction', 0.1);
- options = optimoptions(options,'CrossoverFcn', { @crossoverheuristic, 1});
+% options = optimoptions(options,'ParetoFraction', 0.1);
+% options = optimoptions(options,'CrossoverFcn', { @crossoverheuristic, 1});
 % options = optimoptions(options,'CreationFcn', @gacreationnonlinearfeasible);
 % options = optimoptions(options,'SelectionFcn', {  @selectiontournament [] });
 
@@ -85,51 +85,39 @@ options = optimoptions(options,'UseParallel', false);
 [x,fval,exitflag,output,population,score] = ...
     gamultiobj(@multiobj2,nvars,[],[],[],[],lb,ub,@(x)const(x,Lx,Ly,rho,h,h0,d,Lr),options);
 
-% LC(:,i)=x(:,1);
-% Rg(:,i)=x(:,4);
-% 
-% end
-% 
-% [m nn]=size(LC);
-% LC=reshape(LC,m*nn,1);
-% [m nn]=size(Rg);
-% Rg=reshape(Rg,m*nn,1);
-% 
-% 
-% 
-% fit=fit(LC,Rg,'poly1','normalize','on');
-% 
-% 
-% figure(3)
-% hold on
-% plot(fit,LC,Rg,'predobs');
-% plot(LC,Rg);
-% xlabel('Logitud del Condutor [m]')
-% ylabel('Rg [Ohms]')
-% hold off
-
-
-
-
-figure (2)
-plot(x(:,7),x(:,4),'k*');
-xlabel('Numero de Rods')
-ylabel('# Rg')
-
-%[x,fval,exitflag,output,population,score] = ...
-%    gamultiobj(@multiobj2,nvars,[],[],[],[],lb,ub,@(x)const(x,Lx,Ly,rho,h,h0,d,Lr),options);
-
-% options = optimoptions('paretosearch','UseVectorized',false,'ParetoSetSize',200);
-% rng default % For reproducibility
-% [x,f] = paretosearch(@multiobj,nvars,[],[],[],[],lb,ub,@(x)const(x,Lx,Ly,rho,h,h0,d,Lr),options);
-
-hold on
-%plot(x(:,1),x(:,7),'r*');
-grid on
-hold off
 
 %3D
+figure (1)
+scatter3(x(:,1),x(:,7),x(:,4),'k.');
+
+
+F = scatteredInterpolant(x(:,1),x(:,7),x(:,4),'linear','none');
+
+sgr = linspace(min(x(:,1)),max(x(:,1)));
+ygr = linspace(min(x(:,7)),max(x(:,7)));
+[XX,YY] = meshgrid(sgr,ygr);
+ZZ = F(XX,YY);
+
+figure (2)
+surf(XX,YY,ZZ,'LineStyle','none')
+hold on
+scatter3(x(:,1),round(x(:,7)),x(:,4),'k.');
+xlabel('Longuitud del conductor-LC')
+ylabel('Número de Varillas-nR')
+zlabel('Rg')
+hold off
+
 figure (3)
-scatter3(x(:,7),x(:,4),x(:,6),'k.');
+p = parallelplot(fval);
+p.CoordinateTickLabels =["LC";"nR";"Es"];
+%p.Color = jet(8);
+
+
+% minObj2 = min(fval(:,2));
+% maxObj2 = max(fval(:,2));
+% grpRng = minObj2 + 0.1*(maxObj2-minObj2);
+% grpData = fval(:,2) <= grpRng;
+% p.GroupData = grpData;
+% p.LegendVisible = "off";
 
 
